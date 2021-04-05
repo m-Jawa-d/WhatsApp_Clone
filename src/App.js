c08760f4d0030ae373519130d362e6497edaf820
MuhammadJawad <chudhryjawad@gmail.com>
2021-04-05 12:00:44 +0500

    Whats_App_Clone

--- src/App.js
+++ src/App.js
@@ -1,58 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from 'react'
+import LogIn from './LogIn'
+import NavbarHeader from './NavbarHeader'
+import StartupPage from './StartupPage'
+import { Switch, Route, NavLink } from 'react-router-dom';
+import Chats from './Chats';
+import Status from './Status';
+import Calls from './Calls';
+import NavBar from './NavBar';
+import UserId from './UserId';
+import Cameras from './Cameras';
+import AbdulQadir from './PeoplesChats/AbdulQadir';
+import LatifChat from './PeoplesChats/LatifChat';
+import AsimChat from './PeoplesChats/AsimChat';
+import JawadChat from './PeoplesChats/JawadChat';
+import Messi from './PeoplesChats/Messi';
+import MubashirChat from './PeoplesChats/MubashirChat';
+import PMASChat from './PeoplesChats/PMASChat';
+import QasimChat from './PeoplesChats/QasimChat';
+import TanzeelChat from './PeoplesChats/TanzeelChat';
+import Video from './Video';
+import LogInCamere from './LogInCamere';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+    <Switch>
+      <div className='App_Main_container'>
+        <Route exact path="/"> <StartupPage /></Route>
+        <Route exact path='/Chats'><div style={{ display: 'flex', flexDirection: 'column' }}> <Chats /> </div></Route>
+        <Route exact path='/Status'><div style={{ display: 'flex', flexDirection: 'column' }}> <Status /></div></Route>
+        <Route exact path='/Calls'><div style={{ display: 'flex', flexDirection: 'column' }}><Calls /></div></Route>
+        <Route exact path="/Login"><LogIn/></Route>
+        <Route exact path="/UserId"><UserId/></Route>
+        <Route exact path="/Cameras"><Cameras/></Route>
+        <Route exact path="/LogInCamere"><LogInCamere/></Route>
+        <Route exact path="/Video"><Video/></Route>
+        <Route exact path="/AbdulQadir"><AbdulQadir/></Route>
+        <Route exact path="/AsimChat"><AsimChat/></Route>
+        <Route exact path="/JawadChat"><JawadChat/></Route>
+        <Route exact path="/LatifChat"><LatifChat/></Route>
+        <Route exact path="/Messi"><Messi/></Route>
+        <Route exact path="/MubashirChat"><MubashirChat/></Route>
+        <Route exact path="/QasimChat"><QasimChat/></Route>
+        <Route exact path="/TanzeelChat"><TanzeelChat/></Route>
+        <Route exact path="/PMASChat"><PMASChat/></Route>
+
+      </div>
+    </Switch>
+  )
 }
 
-export default App;
+export default App
